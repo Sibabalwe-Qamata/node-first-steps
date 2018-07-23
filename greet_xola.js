cef9ed43c15dff5a51545818885fd23e497a2176
Sibabalwe Qamata <prof.qamata@gmail.com>
2018-07-23 15:50:40 +0200

    Used figlet and chalk in the node program

--- greet_xola.js
+++ greet_xola.js
@@ -1,10 +1,16 @@
+"use strict";
 const chalk = require('chalk');
+const figlet = require('figlet');
 //import the greet module that is in the current folder
 const greet = require('./greet');
 
 const styledMessage = chalk.bgGreen.black(greet('Xola'));
-console.log(styledMessage)
 
-//const greet = require('./greet');
+console.log(figlet.textSync("Hello, Xola", {
+    font: 'Standard',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+}));
 
-//console.log(greet('Xola'))
+console.log(styledMessage);
+
